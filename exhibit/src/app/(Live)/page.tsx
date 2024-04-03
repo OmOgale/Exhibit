b96d5ced3b5690ca7c00b66f2c24045b20d6b068
OmOgale <123871192+OmOgale@users.noreply.github.com>
2024-04-03 00:24:46 -0400

    Changing icons

--- exhibit/src/app/(Live)/page.tsx
+++ exhibit/src/app/(Live)/page.tsx
@@ -11,7 +11,6 @@ export default function Live() {
     <>
       <Hero bg={"exhibit.200"}>
         <Introduction />
-
       </Hero>
     </>
   );
--- exhibit/src/components/SkipNav.tsx
+++ exhibit/src/components/SkipNav.tsx
@@ -1,25 +1,26 @@
 "use client";
 import { Link as ScrollLink } from "react-scroll";
 import { Link, List, ListItem, ListIcon } from "@chakra-ui/react";
+import { FaGithubAlt } from "react-icons/fa";
 
 const SkipNav = () => {
   return (
     <List spacing={3}>
       <ListItem>
-        <ListIcon as={MdCheckCircle} color="green.500" />
+        <ListIcon as={FaGithubAlt} color="green.500" />
         Lorem ipsum dolor sit amet, consectetur adipisicing elit
       </ListItem>
       <ListItem>
-        <ListIcon as={MdCheckCircle} color="green.500" />
+        <ListIcon as={FaGithubAlt} color="green.500" />
         Assumenda, quia temporibus eveniet a libero incidunt suscipit
       </ListItem>
       <ListItem>
-        <ListIcon as={MdCheckCircle} color="green.500" />
+        <ListIcon as={FaGithubAlt} color="green.500" />
         Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
       </ListItem>
       {/* You can also use custom icons from react-icons */}
       <ListItem>
-        <ListIcon as={MdSettings} color="green.500" />
+        <ListIcon as={FaGithubAlt} color="green.500" />
         Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
       </ListItem>
     </List>
